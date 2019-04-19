c2ce09dd0a931d316e9b16799b8184e6ea2d6c66
AlexanderAnder <33459265+AlexanderAnder@users.noreply.github.com>
2019-04-19 14:53:03 +0200

    lidl changes to motion

--- myscripts.js
+++ myscripts.js
@@ -28,7 +28,7 @@ var deadlyRight = false;
 
 //Startet das Spiel mit dem Spielfeld 
 function startGame() {
-	myGamePiece = new component(10,10,"pink", (window.innerWidth)/2,0);
+	myGamePiece = new component(10,10,"navy", (window.innerWidth)/2,0);
 	gamePieceHeight = myGamePiece.height;
 	gamePieceWidth = myGamePiece.width;
 	checkAcc();
@@ -422,16 +422,16 @@ function setObstacles(){
 //Bewegungssteuerung des Spielsteins
 function deviceMotionListener(event) {
 	if (window.matchMedia("(orientation: landscape)").matches) {
-       if(event.acceleration.y > 0){
-		   myGamePiece.speedX = Math.min(event.acceleration.y/5,maxSpeed);
+       if(event.accelerationIncludingGravity.x > 0){
+		   myGamePiece.speedX = Math.min(event.accelerationIncludingGravity.x/5,maxSpeed);
 	}else{
-     myGamePiece.speedX = Math.max(event.acceleration.y/5,minSpeed);
+     myGamePiece.speedX = Math.max(event.accelerationIncludingGravity.x/5,minSpeed);
 	}
    }else{
-	if (event.acceleration.x > 0){
-	myGamePiece.speedX = Math.min(event.acceleration.x/5,maxSpeed);
+	if (event.accelerationIncludingGravity.y > 0){
+	myGamePiece.speedX = Math.min(event.accelerationIncludingGravity.y/5,maxSpeed);
 	}else{
-     myGamePiece.speedX = Math.max(event.acceleration.x/5,minSpeed);
+     myGamePiece.speedX = Math.max(event.accelerationIncludingGravity.y/5,minSpeed);
 	}
   }
 }
